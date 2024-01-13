1ece88b12ab5840fbe2ecc99bda1d5f034935d50
Srijan Samridh <srijansinha3@gmail.com>
2024-01-13 17:45:26 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { connectDatabase } = require("./config/mongodb");
+// const { getIPAddress } = require("./utils/getIPAddress");
+
+dotenv.config();
+connectDatabase();
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app
+  .use(cors())
+  .set("x-powered-by", false)
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running...",
+  });
+});
+
+app.use("/api/auth", require("./src/routes/auth.js"));
+app.use("/api/friend-request", require("./src/routes/friends.js"));
+
+const text = `
+************************************************************
+                  Listening on port: ${PORT}
+                  http://localhost:${PORT}
+
+************************************************************`;
+
+app.listen(PORT, () => {
+  console.log(text);
+});
---
+++ src/models/user.models.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    bio: { type: String },
+    profilePicture: { type: String },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
